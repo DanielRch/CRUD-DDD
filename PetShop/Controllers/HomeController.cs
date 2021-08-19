1a98b0f9ace543fff5404910578cd1935b63c380
DanielRch <69549252+DanielRch@users.noreply.github.com>
2021-08-18 21:44:35 -0300

    Adicionar arquivos de projeto.

---
+++ PetShop/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Petshop.Infra.Context;
+using PetShop.Models;
+
+namespace PetShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ContextPetShop _db;
+
+        public HomeController(ILogger<HomeController> logger, ContextPetShop db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            
+            _db.Cliente.ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
